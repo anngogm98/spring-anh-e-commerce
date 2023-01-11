e1943ef65ffc35b95fe8c98b860c91339c4e343e
anngo <anngogm98@gmail.com>
2023-01-12 00:04:55 +0700

    init project

---
+++ product-service/src/test/java/com/thaiclone/productservice/ProductServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thaiclone.productservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
